2e80c2534510426aa4b31dffc4b22fdccef06db4
cornelius huang <cornelius.huang@capgemini.com>
2017-03-18 19:27:26 +0800

    first demo not finish

--- formmybatis/src/main/java/com/yunikoro/formmybatis/App.java
+++ formmybatis/src/main/java/com/yunikoro/formmybatis/App.java
@@ -1,13 +1,22 @@
 package com.yunikoro.formmybatis;
 
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) throws Exception {
         System.out.println( "Hello World!" );
+        String resource = "com/yunikoro/formmybatis/config/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        System.out.println(inputStream);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        
     }
 }
