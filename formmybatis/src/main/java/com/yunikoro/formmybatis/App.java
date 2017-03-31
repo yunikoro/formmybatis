d0a907045c2ba899977ad9fc503d710cf6b7fea8
cornelius huang <cornelius.huang@capgemini.com>
2017-03-31 18:59:41 +0800

    reflect    Class.getClasses()

--- formmybatis/src/main/java/com/yunikoro/formmybatis/App.java
+++ formmybatis/src/main/java/com/yunikoro/formmybatis/App.java
@@ -1,10 +1,14 @@
 package com.yunikoro.formmybatis;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import junit.framework.Test;
 
 /**
  * Hello world!
@@ -19,6 +23,27 @@ public class App {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);*/
         
     	Class c = "foo".getClass();
-    	System.out.println(c.getSimpleName());
+    	Class d = Double.class;
+    	Class e = Class.forName("[I");
+    	Class f = int[].class;
+    	Class g = Double.TYPE;
+    	Class h = Integer.TYPE;
+    	
+    	//System.out.println(Object.class.getClasses());
+    	Class[] clses = Character.class.getClasses();
+    	List<Class> lstCls = new ArrayList<Class>();
+    	List<Integer> lstInt = new ArrayList<Integer>();
+    	Integer[] testary = {1, 2, 3};
+    	lstInt.toArray(testary);
+    	System.out.println(lstCls);
+    	lstCls.forEach(cls -> {
+    		//String c = cls.getName();
+    		System.out.println(cls.getName());
+    	});
+    	
+    	lstInt.forEach(cls -> {
+    		//String c = cls.getName();
+    		System.out.println(cls);
+    	});
     }
 }
