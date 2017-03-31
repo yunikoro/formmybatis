a55dd276f9fc34cdf26a5bba0de42f7ea3a34fc2
cornelius huang <cornelius.huang@capgemini.com>
2017-03-31 18:09:51 +0800

    http://docs.oracle.com/javase/tutorial/reflect/class/classNew.html

--- formmybatis/src/main/java/com/yunikoro/formmybatis/App.java
+++ formmybatis/src/main/java/com/yunikoro/formmybatis/App.java
@@ -12,11 +12,13 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println( "Hello World!" );
+        /*System.out.println("Hello World!");
         String resource = "com/yunikoro/formmybatis/config/mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         System.out.println(inputStream);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);*/
         
+    	Class c = "foo".getClass();
+    	System.out.println(c.getSimpleName());
     }
 }
